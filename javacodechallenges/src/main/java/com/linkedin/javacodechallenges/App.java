030be1a644f650e02c0ad4d33e02969c96fa65dd
Xavier Avery <XavierAvery@users.noreply.github.com>
2023-04-25 16:55:00 +0000

    added isEven method

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -6,8 +6,23 @@ package com.linkedin.javacodechallenges;
  */
 public class App 
 {
+    public static boolean isEven(int n){
+        // todo: check if even
+        // if (n % 2 == 0){
+        //     return true;
+        // } else {
+        //     return false;
+        //}
+        return n % 2 == 0;
+         
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println(isEven(2));
+
+        System.out.println(isEven(9));
     }
 }
